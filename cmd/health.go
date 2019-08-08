46416a750ec4781391e658274ce0bcf07b249937
benemenda <ben.marsden@emenda.se>
2019-08-08 19:02:57 +0100

    added descriptions for commands

--- cmd/health.go
+++ cmd/health.go
@@ -35,13 +35,8 @@ type health struct {
 // healthCmd represents the health command
 var healthCmd = &cobra.Command{
 	Use:   "health",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Checks the availability of the configured klocwork web server",
+	Long:  `Checks the availability of the configured klocwork web server`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("health called")
 		ok := config.IsHealthy()
